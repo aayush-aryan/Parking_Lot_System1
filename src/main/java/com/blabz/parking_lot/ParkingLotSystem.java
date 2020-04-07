0333c60ec1ee16dafe024980249eff959393b5a9
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-07 12:47:22 +0530

    [Aayush]added. adding Welcome message to ParkingLotSystem

---
+++ src/main/java/com/blabz/parking_lot/ParkingLotSystem.java
@@ -0,0 +1,8 @@
+package com.blabz.parking_lot;
+
+public class ParkingLotSystem {
+
+        public static void main(String[] args) {
+            System.out.println(" Welcome to ParkingLotSystem");
+        }
+}
