999183639ab69d0805d964a0d605451d0b9f38c2
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-07 13:18:34 +0530

    [Aayush]added. unpark method is added to return boolean value

--- src/main/java/com/blabz/parking_lot/ParkingLotSystem.java
+++ src/main/java/com/blabz/parking_lot/ParkingLotSystem.java
@@ -1,17 +1,23 @@
 package com.blabz.parking_lot;
 
-public class ParkingLotSystem {
-    private Object vehicle;
+public class ParkingLotSystem{
+private Object vehicle;
 
-    public ParkingLotSystem(){
-    }
-    public boolean park(Object vehicle) {
+        public ParkingLotSystem(){
+        }
+public boolean park(Object vehicle) {
         if(this.vehicle!=null){
-            return false;
+        return false;
         }
         this.vehicle=vehicle;
         return true;
-    }
-
-
+        }
+public boolean unPark(Object vehicle) {
+        if (this.vehicle == null) return false;
+        if (this.vehicle.equals(vehicle)) {
+        this.vehicle = null;
+        return true;
+        }
+        return false;
+        }
 }
