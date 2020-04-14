0becc289101d18697263503c80fd520517da9aa3
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-15 01:40:15 +0530

    [Aayush]added.added a test case  for handicaped driver

--- src/test/java/EvenDistributionStrategyTester.java
+++ src/test/java/EvenDistributionStrategyTester.java
@@ -1,14 +1,21 @@
+
 import com.blabz.parking_lot.EvenDistributionParkingStrategy;
-import com.blabz.parking_lot.ParkingLotException;
-import com.blabz.parking_lot.ParkingLots;
-import com.blabz.parking_lot.Vehicle;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import parkinglot.exception.ParkingLotException;
+import parkinglot.parkinglotcomponent.ParkingLots;
+import parkinglot.parkingstrategy.EvenDistributionParkingStrategy;
+import parkinglot.vehicles.DriverType;
+import parkinglot.vehicles.Vehicle;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EvenDistributionStrategyTester {
@@ -17,7 +24,7 @@ public class EvenDistributionStrategyTester {
     @Before
     public void setup(){
         parkingLots = new ParkingLots(4);
-        for(int i=0; i<3; i++)
+        for(int i=1; i<=4; i++)
             this.parkingLotsList.add(new ParkingLots(4));
     }
     @Mock
@@ -27,15 +34,11 @@ public class EvenDistributionStrategyTester {
         when(parkingStrategy.getParkingLot(parkingLotsList)).thenReturn(parkingLotsList.get(0));
         Assert.assertEquals(parkingStrategy.getParkingLot(parkingLotsList),parkingLotsList.get(0));
     }
-
-    private Object when(ParkingLots parkingLot) {
-    }
-
     @Test
-    public void IfDriverParksThreeCarsEachParkingSlotShouldBeOccupiedByOne() throws ParkingLotException {
-            ParkingLots parkingLot1 = parkingLots.getParkingLot(parkingLotsList);
+    public void IfDriverParksTwoCarsEachParkingSlotShouldBeOccupiedByOne() throws ParkingLotException {
+            ParkingLots parkingLot1 = parkingLots.getParkingLot(parkingLotsList, DriverType.HANDICAPPED);
             parkingLot1.park(new Vehicle("car"));
-            ParkingLots parkingLot2 = parkingLots.getParkingLot(parkingLotsList);
+            ParkingLots parkingLot2 = parkingLots.getParkingLot(parkingLotsList,DriverType.HANDICAPPED);
             parkingLot2.park(new Vehicle("car"));
             Integer[] expected = new Integer[]{Integer.valueOf(1), Integer.valueOf(1)};
             Integer[] actual = new Integer[]{parkingLot1.getNumOfVehicleParked(), parkingLot2.getNumOfVehicleParked()};
---
+++ src/test/java/NearestFreeSpaceStrategyTester.java
@@ -0,0 +1,40 @@
+import com.blabz.parking_lot.NearestFreeSpaceStrategy;
+import com.blabz.parking_lot.ParkingLotException;
+import com.blabz.parking_lot.ParkingLots;
+import com.blabz.parking_lot.Vehicle;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NearestFreeSpaceStrategyTester {
+    List<ParkingLots> parkingLotsList = new ArrayList<>();
+    ParkingLots parkingLots;
+    @Before
+    public void setup(){
+        parkingLots = new ParkingLots(4);
+        for(int i=1; i<=4; i++)
+            this.parkingLotsList.add(new ParkingLots(4));
+    }
+    @Mock
+    NearestFreeSpaceStrategy parkingStrategy = new NearestFreeSpaceStrategy();
+    @Test
+    public void givenParkingLotWhenDriverWantsParkedCarShouldReturnFirstParkingLots() throws ParkingLotException, ParkingLotException {
+        when(parkingStrategy.getParkingLot(parkingLotsList)).thenReturn(parkingLotsList.get(0));
+        Assert.assertEquals(parkingStrategy.getParkingLot(parkingLotsList),parkingLotsList.get(0));
+    }
+    @Test
+    public void IfDriverParksTWoCarsBothCarParkedShouldInFirstSlot() throws ParkingLotException {
+        ParkingLots parkingLot1 = parkingLots.getParkingLot(parkingLotsList, DriverType.HANDICAPPED);
+        parkingLot1.park(new Vehicle("car"));
+        ParkingLots parkingLot2 = parkingLots.getParkingLot(parkingLotsList,DriverType.HANDICAPPED);
+        parkingLot2.park(new Vehicle("car"));
+        Integer[] expected = new Integer[]{Integer.valueOf(2), Integer.valueOf(2)};
+        Integer[] actual = new Integer[]{parkingLot1.getNumOfVehicleParked(), parkingLot2.getNumOfVehicleParked()};
+        Assert.assertArrayEquals(expected,actual);
+    }
+}
